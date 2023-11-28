40f6371bfec982443bf23a074068a1e5bca8f89a
PedroElric <138056745+pedroelr1c@users.noreply.github.com>
2023-11-28 19:32:30 +0000

    aula 1

---
+++ Unidade 1/aula1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* 
+
+Aprender a depurar.
+-g é usado para depurar
+step você entra em uma função.
+where mostra onde está.
+
+ */
+int main(void){
+
+    printf("Hello hord!\n");
+
+    return 0;
+}
